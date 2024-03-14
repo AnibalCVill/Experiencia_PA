5085a2ec654d5b668b1ce79f814c8c1fd0ee45f4
AnibalCVill <141983919+AnibalCVill@users.noreply.github.com>
2024-03-14 15:30:55 +0000

    juego 1 listo

--- juegos/adivinar_numero.py
+++ juegos/adivinar_numero.py
@@ -1,7 +1,11 @@
+from random import randint
+
 def adivinar_numero():
-    """
-    Esta función representa el juego de adivinar un número.
-    Debes generar un número al azar entre 1 y 10, y luego pedir al usuario que adivine el número.
-    Se debe mostrar un mensaje si el usuario adivina correctamente o no.
-    """
-    pass
+    num=randint(1,10)
+
+    print("Ingrese su numero")
+    numero_ingresar = input()
+    if numero_ingresar == num:
+        print("Felicidades, adivinaste el numero")
+    else:
+        print("Mal, era el", num)
