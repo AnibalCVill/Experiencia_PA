c4257fe540f123a7c08c58b39ce3a72e16faa6d9
TFUEntealba <163364964+TFUEntealba@users.noreply.github.com>
2024-03-14 13:35:17 +0000

    cachipun listo

--- juegos/cachipun.py
+++ juegos/cachipun.py
@@ -1,7 +1,24 @@
+import random
 def cachipun():
     """
     Esta función representa el juego de cachipun.
     Debes pedir al usuario que elija piedra, papel o tijera, y luego comparar su elección con la de la computadora.
     La computadora debe elegir una opción al azar.
     """
+
+    usuario = str(input('Ingrese "PIEDRA", "PAPEL" o "TIJERA"'))
+    lista = ['PIEDRA', 'PAPEL', 'TIJERA']
+    comp = random.choice(lista)
+
+    if usuario == comp:
+        print('Empataste')
+    else:
+        if usuario == 'PIEDRA' and comp == 'PAPEL':
+            print(f'Perdiste, la computadora eligio {comp}')
+        elif usuario == 'TIJERA' and comp == 'PIEDRA':
+            print(f'Perdiste, la computadora eligio {comp}')
+        elif usuario == 'PAPEL' and comp == 'TIJERA':
+            print(f'Perdiste, la computadora eligio {comp}')
+        else:
+            print(f'Ganaste, la computadora eligio {comp}')
     pass
